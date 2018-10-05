31c02a6d0b94afb908d16db1bac6bbcac892af41
Yu Li <yli0607x@gmail.com>
2018-10-05 02:55:01 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,10 +18,10 @@ def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   else
-  current_line =[]
+  current_line ="The line is currently:"
   katz_deli.each.with_index do |name, i|
   current_line.push(" #{i+1}. #{name}")
-  puts "The line is currently: #{current_line}"
+  puts current_line
 end
 end
 end
